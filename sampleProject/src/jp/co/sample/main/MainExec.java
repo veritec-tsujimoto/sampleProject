4e30be5fd81a04020125b47542a1916117ee2c73
veritec-tsujimoto <veritec.tsujimoto@gmail.com>
2017-08-10 09:43:00 +0900

    サンプルプロジェクト初期登録

---
+++ sampleProject/src/jp/co/sample/main/MainExec.java
@@ -0,0 +1,25 @@
+package jp.co.sample.main;
+
+/**
+ * 起動クラス
+ *
+ */
+public class MainExec {
+
+	/**
+	 * コンストラクタ.
+	 */
+	public MainExec() {
+
+	}
+
+	/**
+	 * メインメソッド
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
